3b2c4811c203154ebb788ec9840b718734b0554a
jmaheras <maheras.3@buckeyemail.osu.edu>
2025-12-07 20:40:36 -0500

    change ipaddress

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ from socket import *
 import tkinter as tk
 from tkinter import scrolledtext, simpledialog
 
-SERVER_IP = "172.28.211.68"
+SERVER_IP = "0.0.0.1"
 SERVER_PORT = 12001
 
 #create TCP client Socket
